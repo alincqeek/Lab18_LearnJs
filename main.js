06750903a6033529b03737100d5e5586f0f0649a
alincqeek <a72658695@gmail.com>
2026-02-26 20:43:37 +0300

    Added complete README for Lab18

--- main.js
+++ main.js
@@ -155,32 +155,32 @@
 // let b3 = "10";
 // console.log(a3 == b3)
 // console.log(a3 === b3)
-№4 
-let monthNumber=3;
-switch(monthNumber){
-    case 1: console.log("Январь");
-        break;
-    case 2: console.log("Февраль");
-        break;
-    case 3: console.log("Март");
-        break;
-    case 4: console.log("Апрель");
-        break;
-    case 5: console.log("Май");
-        break;
-    case 6: console.log("Июнь");
-        break;
-    case 7: console.log("Июль");
-        break;
-    case 8: console.log("Август");
-        break;
-    case 9: console.log("Сентябрь");
-        break;
-    case 10: console.log("Октябрь");
-        break;
-    case 11: console.log("Ноябрь");
-        break;
-    case 12: console.log("Декабрь");
-        break;
-    default: console.log("Нет такого месяца");
-}
+// №4 
+// let monthNumber=3;
+// switch(monthNumber){
+//     case 1: console.log("Январь");
+//         break;
+//     case 2: console.log("Февраль");
+//         break;
+//     case 3: console.log("Март");
+//         break;
+//     case 4: console.log("Апрель");
+//         break;
+//     case 5: console.log("Май");
+//         break;
+//     case 6: console.log("Июнь");
+//         break;
+//     case 7: console.log("Июль");
+//         break;
+//     case 8: console.log("Август");
+//         break;
+//     case 9: console.log("Сентябрь");
+//         break;
+//     case 10: console.log("Октябрь");
+//         break;
+//     case 11: console.log("Ноябрь");
+//         break;
+//     case 12: console.log("Декабрь");
+//         break;
+//     default: console.log("Нет такого месяца");
+// }
